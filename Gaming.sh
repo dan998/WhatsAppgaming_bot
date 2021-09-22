97b760e1afebeedc7749b5195f5ee71dc0322bef
dan998 <73266204+dan998@users.noreply.github.com>
2021-09-21 20:33:31 -0400

    Add files via upload

---
+++ Gaming.sh
@@ -0,0 +1,15 @@
+pkg update && pkg upgrade
+
+pkg install git -y
+
+pkg install nodejs -y
+
+pkg install ffmpeg -y
+
+pkg install imagemagick -y
+
+git clone https://github.com/BochilGaming/games-wabot
+
+cd games-wabot
+
+npm install
